484b96bbf85780b73e7295a27fa28b27dcd384d7
SEG91 <50146121+SEG91@users.noreply.github.com>
2023-10-07 17:53:44 +0200

    update template

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -2,7 +2,7 @@
  * Extend the base Actor document by defining a custom roll data structure which is ideal for the Simple system.
  * @extends {Actor}
  */
-export class BoilerplateActor extends Actor {
+export class ValombreuseActor extends Actor {
 
   /** @override */
   prepareData() {
@@ -31,7 +31,7 @@ export class BoilerplateActor extends Actor {
   prepareDerivedData() {
     const actorData = this;
     const systemData = actorData.system;
-    const flags = actorData.flags.boilerplate || {};
+    const flags = actorData.flags.valombreuse || {};
 
     // Make separate methods for each Actor type (character, npc, etc.) to keep
     // things organized.
@@ -43,7 +43,7 @@ export class BoilerplateActor extends Actor {
    * Prepare Character type specific data
    */
   _prepareCharacterData(actorData) {
-    if (actorData.type !== 'character') return;
+    if (actorData.type !== 'lignee') return;
 
     // Make modifications to data here. For example:
     const systemData = actorData.system;
@@ -63,7 +63,6 @@ export class BoilerplateActor extends Actor {
 
     // Make modifications to data here. For example:
     const systemData = actorData.system;
-    systemData.xp = (systemData.cr * systemData.cr) * 100;
   }
 
   /**
